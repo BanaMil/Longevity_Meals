54a815593125989da6f56106ebee4cb280d0c6b7
Han <ddalu1223@gmail.com>
2025-05-16 05:08:40 +0000

    change

--- backend/src/main/java/com/capstone/backend/service/TesseractService.java
+++ backend/src/main/java/com/capstone/backend/service/TesseractService.java
@@ -12,7 +12,8 @@ public class TesseractService {
     public String extractTextFromImage(File imageFile) {
         Tesseract tesseract = new Tesseract();
         tesseract.setDatapath("/usr/share/tesseract-ocr/4.00/tessdata"); // 경로는 환경에 맞게 설정
-        tesseract.setLanguage("kor"); // 한국어 인식
+        tesseract.setLanguage("kor+eng"); // 한국어 인식
+        tesseract.setPageSegMode(11);
 
         try {
             return tesseract.doOCR(imageFile);
