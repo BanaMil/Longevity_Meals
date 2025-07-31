48623372466e7b1006e17102330e49f087569f8d
BanaMil <ddalu1223@gmail.com>
2025-07-31 20:57:19 +0900

    영운 timeout 설정

--- backend/src/main/java/com/capstone/backend/config/RestTemplateConfig.java
+++ backend/src/main/java/com/capstone/backend/config/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package com.capstone.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +11,9 @@ public class RestTemplateConfig {
     
     @Bean
     public RestTemplate restTemplate() {
+        var factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);  // ms
+        factory.setReadTimeout(5000);     // ms
         return new RestTemplate();
     }
 }
