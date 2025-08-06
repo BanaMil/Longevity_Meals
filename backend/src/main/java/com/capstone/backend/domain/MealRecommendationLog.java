64c019f330478fc65449a31e5da6fec807750fa2
BanaMil <ddalu1223@gmail.com>
2025-08-06 16:10:28 +0900

    MealRecommendationLog 식단정보 추가

--- backend/src/main/java/com/capstone/backend/domain/MealRecommendationLog.java
+++ backend/src/main/java/com/capstone/backend/domain/MealRecommendationLog.java
@@ -19,12 +19,15 @@ public class MealRecommendationLog {
     private String id;
     private String userId;
     private LocalDate date;             // 추천된 날짜
-    // private List<FoodWithIntake> breakfast;
-    // private List<FoodWithIntake> lunch;
-    // private List<FoodWithIntake> dinner;
+    private List<FoodWithIntake> breakfast;
+    private List<FoodWithIntake> lunch;
+    private List<FoodWithIntake> dinner;
 
-    public MealRecommendationLog(String userid) {
+    public MealRecommendationLog(String userid, List<FoodWithIntake> breakfast, List<FoodWithIntake> lunch, List<FoodWithIntake> dinner) {
         this.userId = userid;
         this.date = LocalDate.now();  // 기본 추천일
+        this.breakfast = breakfast;
+        this.lunch = lunch;
+        this.dinner = dinner;
     }
 }
