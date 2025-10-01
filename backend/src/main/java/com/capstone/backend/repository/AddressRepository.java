0995f0c7ae8b748f0a18536490b2f40ab54d0818
BanaMil <ddalu1223@gmail.com>
2025-10-01 19:21:56 +0900

    AddressRepository 쓸데없는 import 삭제

--- backend/src/main/java/com/capstone/backend/repository/AddressRepository.java
+++ backend/src/main/java/com/capstone/backend/repository/AddressRepository.java
@@ -1,6 +1,5 @@
 package com.capstone.backend.repository;
 import com.capstone.backend.domain.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
