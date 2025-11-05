5b6698af693b413b3b930e80654aaf7ca0a9d575
BanaMil <ddalu1223@gmail.com>
2025-11-05 19:13:19 +0900

    LoginResponse postCode 추가

--- backend/src/main/java/com/capstone/backend/dto/LoginResponse.java
+++ backend/src/main/java/com/capstone/backend/dto/LoginResponse.java
@@ -14,16 +14,18 @@ public class LoginResponse {
     private String addressJibun;
     private String addressRoad;
     private String addressDetail;
+    private String postCode;
     private List<Address> addresses;
     private String token;
     private boolean healthInfoSubmitted;
 
-    public LoginResponse(String userid, String username, String address, String addressJibun, String addressRoad, String addressDetail, List<Address> addresses, String token, boolean healthInfoSubmitted) {
+    public LoginResponse(String userid, String username, String address, String addressJibun, String addressRoad, String postCode, String addressDetail, List<Address> addresses, String token, boolean healthInfoSubmitted) {
         this.userid = userid;
         this.username = username;
         this.address = address;
         this.addressJibun = addressJibun;
         this.addressRoad = addressRoad;
+        this.postCode = postCode;
         this.addressDetail = addressDetail;
         this.addresses = addresses;
         this.token = token;
